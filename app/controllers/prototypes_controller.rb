34d008576c3c02ea36d65ff259eeabfa5e384df2
rikumia <nori_1234@icloud.com>
2021-12-06 13:43:56 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -51,8 +51,7 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    @prototype = Prototype.find(params[:id])
-    unless user_signed_in? && current_user.id == @prototype.user_id
+    unless user_signed_in?
      redirect_to action: :index
     end
   end
